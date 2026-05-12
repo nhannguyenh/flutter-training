fd89006e5e7d47c01940a46e96352d0fc91d0da8
Nhan Nguyen Hoang <nhannguyenhoang.hcmus@gmail.com>
2026-05-12 18:44:42 +0700

    Implement Account Details section in User Profile screen

--- flutter/lib/presentation/screen/profile_screen.dart
+++ flutter/lib/presentation/screen/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app/presentation/config/app_colors.dart';
+import 'package:shopping_app/presentation/config/app_font_sizes.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -15,7 +16,7 @@ class ProfileScreen extends StatelessWidget {
         titleTextStyle: TextStyle(
           fontWeight: FontWeight.bold,
           color: Colors.black,
-          fontSize: 20,
+          fontSize: AppFontSizes.fontSize_20,
         ),
         title: Text("Profile Settings"),
         actions: [
@@ -25,12 +26,18 @@ class ProfileScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
+            SizedBox(height: 8),
             // Avatar section
             Center(
               child: Container(
                 width: double.infinity,
                 alignment: Alignment.center,
-                margin: EdgeInsets.all(12),
+                margin: EdgeInsets.only(
+                  left: 16,
+                  right: 16,
+                  top: 8,
+                  bottom: 8
+                ),
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(12)
@@ -101,12 +108,156 @@ class ProfileScreen extends StatelessWidget {
                         style: TextStyle(
                           color: AppColors.primaryColor,
                           fontWeight: FontWeight.bold,
-                          fontSize: 14
+                          fontSize: AppFontSizes.fontSize_14
                         ),
                       ),
                     )
                   ],
                 ),
+              ),
+            ),
+
+            // Account Detail
+            Container(
+              margin: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12)
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8, bottom: 8),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            margin: EdgeInsets.only(left: 16),
+                            child: Text(
+                              "Account Details",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: AppFontSizes.fontSize_20
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            alignment: Alignment.centerRight,
+                            margin: EdgeInsets.only(right: 8),
+                            child: TextButton(
+                              onPressed: () {},
+                              child: Text(
+                                "Edit Details",
+                                style: TextStyle(
+                                  color: AppColors.primaryColor,
+                                  fontSize: AppFontSizes.fontSize_16
+                                ),
+                              )
+                            )
+                          )
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(left: 16, bottom: 8),
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "EMAIL ADDRESS",
+                      style: TextStyle(
+                        color: Colors.grey.shade600,
+                        fontSize: AppFontSizes.fontSize_14
+                      ),
+                    )
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 16, right: 16),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(
+                          vertical: 12,
+                          horizontal: 12
+                        ),
+                        hintStyle: TextStyle(
+                          color: Colors.grey.shade600
+                        ),
+                        hintText: "john.doe@example.com",
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey.shade200),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(top: 8, left: 16),
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "FIRST NAME",
+                      style: TextStyle(
+                        color: Colors.grey.shade600,
+                        fontSize: AppFontSizes.fontSize_14
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(top: 8, left: 32),
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "John",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: AppFontSizes.fontSize_16
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(top: 8, left: 16),
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "LAST NAME",
+                      style: TextStyle(
+                        color: Colors.grey.shade600,
+                        fontSize: AppFontSizes.fontSize_14
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(top: 8, left: 32),
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Doe",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: AppFontSizes.fontSize_16
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(top: 8, left: 16),
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "AGE",
+                      style: TextStyle(
+                        color: Colors.grey.shade600,
+                        fontSize: AppFontSizes.fontSize_14
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(top: 8, left: 32),
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "28",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: AppFontSizes.fontSize_16
+                      ),
+                    ),
+                  ),
+                ],
               ),
             )
           ],
