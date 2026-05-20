1d03fd6eab85ae016bf57358bedfb2ec11d98310
Nhan Nguyen Hoang <nhannguyenhoang.hcmus@gmail.com>
2026-05-20 15:23:14 +0700

    Save User Profile into local Sqlife Database

--- flutter/lib/data/repositories/user_repository.dart
+++ flutter/lib/data/repositories/user_repository.dart
@@ -1,16 +1,28 @@
 import 'package:injectable/injectable.dart';
 import 'package:shopping_app/data/datasources/user_remote_datasource.dart';
+import 'package:shopping_app/data/helpers/db_helper.dart';
 import 'package:shopping_app/data/models/user_profile_model.dart';
 import 'package:shopping_app/domain/repositories/user_repository_interface.dart';
 
 @LazySingleton(as: IUserRepository)
 class UserRepository implements IUserRepository {
   final IUserRemoteDataSource remoteDataSource;
+  final DbHelper dbHelper;
 
-  UserRepository({required this.remoteDataSource});
+  UserRepository({required this.remoteDataSource, required this.dbHelper});
 
   @override
   Future<UserProfileModel> getUserProfile() async {
-    return await remoteDataSource.getUserProfile();
+    try {
+      UserProfileModel userProfile = await remoteDataSource.getUserProfile();
+      await dbHelper.saveUserProfile(userProfile);
+      return userProfile;
+    } catch (e) {
+      final localUserProfile = await dbHelper.getUserProfile();
+      if (localUserProfile != null) {
+        return localUserProfile;
+      }
+      throw Exception(e);
+    }
   }
 }
